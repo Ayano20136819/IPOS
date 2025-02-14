f2481a8e14b496a8cdcb5d80683d523ac2945366
20136819 <20136819@tafe.wa.edu.au>
2025-02-14 13:39:45 +0800

    activity1

--- week2/activity_1/greeter.py
+++ week2/activity_1/greeter.py
@@ -4,13 +4,21 @@ def greet():
     print("Hello, World!")
 
 # create a calculator class and re-use this by instantiating it in the main.py
-
 # So as not to contaminate your global namespace, it is good practice to use a main function.
-
 # so that we can reuse our code as both a module and a script
 # we can use an if statement to ensure that some code is only executed when name is __main__
-
 # module name when imported
+class Calculator:
+    def __init__(self, num1, num2):
+        self.num1 = num1
+        self.num2 = num2
+
+    def sum(self):
+        return self.num1 + self.num2
+
+
+
+
 
     # above is __main__ when running directly
     # only called when a script
--- week2/activity_1/main.py
+++ week2/activity_1/main.py
@@ -1,12 +1,15 @@
 # a basic import
+from greeter import Calculator
 
 # use an alias - consider why might we do this?
-
 # sometimes we only want to import what we need
-
 # create a calculator class in the module
-
 # use the new class to return a score to a user
 
+calculator = Calculator(2, 7)
+result = calculator.sum()
+print(f"The sum is {result}")
 
 
+
+git
